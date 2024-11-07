f3fdb3af66b98bbb892c91ee2846d5694a69a1b4
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2024-11-08 02:45:03 +0300

    second commit

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
 
   return (
     <>
       <div className='App'>
-        <h3>hello</h3>
+        <Home />
       </div>
     </>
   )
